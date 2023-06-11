4a8c84ac1bbf35665a46e66e3ce0a74d797e1238
filipditrich <ditrich@nfctron.com>
2023-06-11 17:19:06 +0200

    feat(pages/index): wip on index page with seating map, connect to mock API
    Signed-off-by: filipditrich <ditrich@nfctron.com>

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,14 @@
-import { AppShell, Container, Header } from '@mantine/core';
+import { AppShell, Button, Container, Header } from '@mantine/core';
+import { useViewportSize } from '@mantine/hooks';
+import { apiClient } from '@project/lib/features/api-client/api-client';
 import { useFormat } from '@project/lib/features/formatter/useFormat';
+import { SeatingMap, SeatingMapTypes } from '@project/lib/features/map/SeatingMap';
+import { SeatingMapProvider } from '@project/lib/features/map/SeatingMap/SeatingMap.context';
 import { PageComponent, PageParams, PageQuery } from '@project/lib/features/next/next.module';
+import { useSuspenseQuery } from '@project/lib/hooks/useApiQuery';
+import { useNow } from '@project/lib/hooks/useNow';
+import { fakePromise, noop, valOrThrow } from '@project/lib/utils/common';
+import { venueApiSchema } from '@project/pages/api/venue/schema';
 import { useRouter } from 'next/router';
 import React, { createContext } from 'react';
 
@@ -14,43 +22,100 @@ type IndexPageContextProps = {};
 
 /**
  * Main index page view component
- * @author filipditrich <ditrich@nfctron.com>
+ * @author filipditrich <filip.ditirch@plus4u.net>
  * @returns {JSX.Element}
  * @constructor
  */
-const IndexPage: PageComponent<Props> = ({}) => {
+const IndexPage: PageComponent<Props> = () => {
 	const router = useRouter();
 	const format = useFormat();
-	
+	const { height, width } = useViewportSize();
+	const now = useNow();
+
+	/** get seating data from API */
+	const venueApi = useSuspenseQuery(
+		['venue', {}],
+		async () => {
+			await fakePromise(1000);
+			return apiClient.get('/api/venue');
+		},
+		{
+			select: (data) => {
+				const rawData = venueApiSchema.parse(data);
+				const extendedData: SeatingMapTypes.DataExtended = {
+					...rawData,
+					seats: rawData.seats.map((seat) => ({
+						...seat,
+						category: valOrThrow(
+							rawData.categories.find((c) => c.categoryId === seat.categoryId),
+							`Category with id ${seat.categoryId} not found`,
+						),
+						tickets: seat.tickets.map((ticketId) => {
+							const ticket = valOrThrow(
+								rawData.tickets.find((t) => t.ticketId === ticketId),
+								`Ticket with id ${ticketId} not found`,
+							);
+							return {
+								...ticket,
+								categories: ticket.categories
+									.filter((ticketCategory) => ticketCategory.categoryId === seat.categoryId)
+									.map((ticketCategory) => {
+										const category = valOrThrow(
+											rawData.categories.find((c) => c.categoryId === ticketCategory.categoryId),
+											`Category with id ${ticketCategory.categoryId} not found`,
+										);
+										return {
+											...ticketCategory,
+											...category,
+										};
+									}),
+							};
+						}),
+					})),
+				};
+				return {
+					raw: rawData,
+					extended: extendedData,
+				};
+			},
+		},
+	);
+
 	return (
 		<React.Fragment>
 			{/* content */}
 			<IndexPageContext.Provider value={{}}>
 				<AppShell
 					padding={0}
-					header={<Header height={60} p="xs">
-						TODO: header
-					</Header>}
+					header={
+						<Header height={75} p="md">
+							<Container size="lg" className="flex items-center gap-3">
+								<span>{venueApi.data.extended.name}</span>
+								<Button className="ml-auto" onClick={() => venueApi.refetch()} loading={venueApi.isRefetching}>
+									Reload
+								</Button>
+							</Container>
+						</Header>
+					}
 					styles={{
 						main: {
 							backgroundColor: 'rgba(0,0,0,0.05)',
-							// minHeight: 'unset !important',
-							paddingBottom: '0 !important'
-						}
+							paddingBottom: '0 !important',
+						},
 					}}
 				>
 					<React.Fragment>
 						{/* main container */}
-						<Container size="xs" px={0} className="flex flex-col">
-							TODO: content
-						</Container>
+						<SeatingMapProvider data={venueApi.data.raw} dataExtended={venueApi.data.extended}>
+							{/* seating map */}
+							<SeatingMap width={width} height={height - 75} minScaleFactor={1.5} maxScaleFactor={0.1} />
+						</SeatingMapProvider>
 					</React.Fragment>
 				</AppShell>
 			</IndexPageContext.Provider>
 		</React.Fragment>
 	);
 };
-
 /**
  * Page display name
  * @type {string}
