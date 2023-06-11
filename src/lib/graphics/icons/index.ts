779b46b74d7fae2b91b8f5f9b9cca4330b2166ef
filipditrich <ditrich@nfctron.com>
2023-06-12 00:28:11 +0200

    feat(lib/graphics): add various brand logo SVG assets
    Signed-off-by: filipditrich <ditrich@nfctron.com>

---
+++ src/lib/graphics/icons/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 🍏 Various brand logo icons icons barrel exports
+ * BarrelGun: auto-generated barrel file, do not edit!
+ */
+
+export { default as PaypalSmallLogo } from './paypal-small.svg';
+export { default as MastercardSmallLogo } from './mastercard-small.svg';
+export { default as ApplePaySmallLogo } from './apple-pay-small.svg';
